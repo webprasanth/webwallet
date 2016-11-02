917a0f667c9a3c7517713cf8a39fdea75c6c9e68
Ba Ban <sandgrain@unseen.is>
2016-11-02 08:43:20 +0700

    Replace jQuery lib and format util code

--- src/model/utils.ts
+++ src/model/utils.ts
@@ -1,211 +1,214 @@
 import Big from 'big.js';
 import _tmp from 'moment-timezone';
 import bitcoin from 'bitcoinjs-lib';
-import { Address, NETWORK } from './wallet';
+import {
+    Address,
+    NETWORK
+} from './wallet';
 
 interface UserKey {
-  idToken: string;
-  encryptedPrivKey: string;
-  publicKey: string;
+    idToken: string;
+    encryptedPrivKey: string;
+    publicKey: string;
 }
 
 const MOMENT_FORMAT = {
-  DATE: "MMM DD, YYYY",
-  DATE_TIME: "MMM DD, YYYY hh:mm A",
-  DATE_TIME_2: "MMM DD, YYYY hh:mm:ss A"
+    DATE: "MMM DD, YYYY",
+    DATE_TIME: "MMM DD, YYYY hh:mm A",
+    DATE_TIME_2: "MMM DD, YYYY hh:mm:ss A"
 }
 
 declare class Buffer extends Object {
-  constructor(arr?: any, encode?: any);
-  toString(encode?: string);
+    constructor(arr ? : any, encode ? : any);
+    toString(encode ? : string);
 }
 
 export function satoshiToFlash(num) {
-  if (num == undefined || num === '') return;
-  return parseFloat(new Big(num).div(10000000).toString());
+    if (num == undefined || num === '') return;
+    return parseFloat(new Big(num).div(10000000).toString());
 }
 
 export function flashToSatoshi(num) {
-  if (num == undefined || num === '') return;
-  return parseInt(new Big(num).times(10000000).toString(), 10);
+    if (num == undefined || num === '') return;
+    return parseInt(new Big(num).times(10000000).toString(), 10);
 }
 
 export function storeUserKey(userKey: UserKey) {
-  if (userKey) {
-    localStorage.setItem("scuserkeys", JSON.stringify(userKey));
-  }
+    if (userKey) {
+        localStorage.setItem("scuserkeys", JSON.stringify(userKey));
+    }
 }
 
 export function removeUserKey() {
-  localStorage.removeItem('scuserkeys');
+    localStorage.removeItem('scuserkeys');
 }
 
 export function getUserKey(): UserKey | any {
-  let str = localStorage.getItem('scuserkeys');
-  let userKey = null;
-  if (str && str.length > 0) {
-    userKey = JSON.parse(str);
-  }
-  return userKey;
+    let str = localStorage.getItem('scuserkeys');
+    let userKey = null;
+    if (str && str.length > 0) {
+        userKey = JSON.parse(str);
+    }
+    return userKey;
 }
 
 export function calcFee(amount) {
-  return 1;
+    return 1;
 }
 
 export function formatCurrency(amount) {
-  return `${amount} Flash`;
+    return `${amount} Flash`;
 }
 
 export function getDisplayDate(date, toTimeZone) {
-  if (toTimeZone && _tmp.tz.zone(toTimeZone) != null)
-    return _tmp(date).tz(toTimeZone).format(MOMENT_FORMAT.DATE);
-  return _tmp(date).local().format(MOMENT_FORMAT.DATE);
+    if (toTimeZone && _tmp.tz.zone(toTimeZone) != null)
+        return _tmp(date).tz(toTimeZone).format(MOMENT_FORMAT.DATE);
+    return _tmp(date).local().format(MOMENT_FORMAT.DATE);
 }
 
 export function getDisplayDateTime(date, toTimeZone) {
-  if (toTimeZone && _tmp.tz.zone(toTimeZone) != null)
-    return _tmp(date).tz(toTimeZone).format(MOMENT_FORMAT.DATE_TIME_2);
-  return _tmp(date).local().format(MOMENT_FORMAT.DATE_TIME_2);
+    if (toTimeZone && _tmp.tz.zone(toTimeZone) != null)
+        return _tmp(date).tz(toTimeZone).format(MOMENT_FORMAT.DATE_TIME_2);
+    return _tmp(date).local().format(MOMENT_FORMAT.DATE_TIME_2);
 }
 
 export function setLocation(location) {
-  localStorage.setItem('location', JSON.stringify(location));
-  this.location = location;
+    localStorage.setItem('location', JSON.stringify(location));
+    this.location = location;
 }
 
 export function getLocation() {
-  let str = localStorage.getItem('location');
-  let location = null;
-  if (str && str.length > 0) {
-    location = JSON.parse(str);
-  }
-  return location;
+    let str = localStorage.getItem('location');
+    let location = null;
+    if (str && str.length > 0) {
+        location = JSON.parse(str);
+    }
+    return location;
 }
 
 export function calcPasswordStreng(password: string): number {
 
-  if (!password) {
-    ;
-    return;
-  }
+    if (!password) {;
+        return;
+    }
 
-  // Initial strength
-  let strength = 0;
+    // Initial strength
+    let strength = 0;
 
-  // If the password length is less than 6, return message.
-  if (password.length < 6) {
+    // If the password length is less than 6, return message.
+    if (password.length < 6) {
+        return strength;
+    }
+
+    // length is ok, lets continue.
+
+    // If length is 8 characters or more, increase strength value
+    if (password.length > 7) strength += 1;
+
+    // If password contains both lower and uppercase characters, increase strength value
+    if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) strength += 1;
+
+    // If it has numbers and characters, increase strength value
+    if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/)) strength += 1;
+
+    // If it has one special character, increase strength value
+    if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) strength += 1;
+
+    // If it has two special characters, increase strength value
+    if (password.match(/(.*[!,%,&,@,#,$,^,*,?,_,~].*[!,%,&,@,#,$,^,*,?,_,~])/)) strength += 1;
+
     return strength;
-  }
-
-  // length is ok, lets continue.
-
-  // If length is 8 characters or more, increase strength value
-  if (password.length > 7) strength += 1;
-
-  // If password contains both lower and uppercase characters, increase strength value
-  if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) strength += 1;
-
-  // If it has numbers and characters, increase strength value
-  if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/)) strength += 1;
-
-  // If it has one special character, increase strength value
-  if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) strength += 1;
-
-  // If it has two special characters, increase strength value
-  if (password.match(/(.*[!,%,&,@,#,$,^,*,?,_,~].*[!,%,&,@,#,$,^,*,?,_,~])/)) strength += 1;
-
-  return strength;
 }
 
 // Base64 to unicode string
 export function b64DecodeUnicode(str: string) {
-  return decodeURIComponent(Array.prototype.map.call(atob(str), function (c) {
-    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join(''));
+    return decodeURIComponent(Array.prototype.map.call(atob(str), function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
 }
 
 export function hexToBase64(str: string) {
-  return btoa(String.fromCharCode.apply(null,
-    str.replace(/\r|\n/g, "").replace(/([\da-fA-F]{2}) ?/g, "0x$1 ").replace(/ +$/, "").split(" "))
-  );
+    return btoa(String.fromCharCode.apply(null,
+        str.replace(/\r|\n/g, "").replace(/([\da-fA-F]{2}) ?/g, "0x$1 ").replace(/ +$/, "").split(" ")));
 }
 
 export function base64ToHex(str: string) {
-  for (var i = 0, bin = atob(str.replace(/[ \r\n]+$/, "")), hex = []; i < bin.length; ++i) {
-    var tmp = bin.charCodeAt(i).toString(16);
-    if (tmp.length === 1) tmp = "0" + tmp;
-    hex[hex.length] = tmp;
-  }
-  return hex.join('');
+    for (var i = 0, bin = atob(str.replace(/[ \r\n]+$/, "")), hex = []; i < bin.length; ++i) {
+        var tmp = bin.charCodeAt(i).toString(16);
+        if (tmp.length === 1) tmp = "0" + tmp;
+        hex[hex.length] = tmp;
+    }
+    return hex.join('');
 }
 
 export function strFromUtf8Ab(ab) {
-  return decodeURIComponent(escape(String.fromCharCode.apply(null, ab)));
+    return decodeURIComponent(escape(String.fromCharCode.apply(null, ab)));
 }
 
 export function decodeBase64(s) {
-  if (typeof atob === 'undefined') {
-    return new Uint8Array(Array.prototype.slice.call(new Buffer(s, 'base64'), 0));
-  } else {
-    var i, d = atob(s), b = new Uint8Array(d.length);
-    for (i = 0; i < d.length; i++) b[i] = d.charCodeAt(i);
-    return b;
-  }
+    if (typeof atob === 'undefined') {
+        return new Uint8Array(Array.prototype.slice.call(new Buffer(s, 'base64'), 0));
+    } else {
+        var i, d = atob(s),
+            b = new Uint8Array(d.length);
+        for (i = 0; i < d.length; i++) b[i] = d.charCodeAt(i);
+        return b;
+    }
 }
 
 export function encodeBase64(arr) {
-  if (typeof btoa === 'undefined') {
-    return (new Buffer(arr)).toString('base64');
-  } else {
-    var i, s = [], len = arr.length;
-    for (i = 0; i < len; i++) s.push(String.fromCharCode(arr[i]));
-    return btoa(s.join(''));
-  }
+    if (typeof btoa === 'undefined') {
+        return (new Buffer(arr)).toString('base64');
+    } else {
+        var i, s = [],
+            len = arr.length;
+        for (i = 0; i < len; i++) s.push(String.fromCharCode(arr[i]));
+        return btoa(s.join(''));
+    }
 }
 
 /**
  * Cut string s if s.length > n
  */
 export function strimString(s, n) {
-  if (s) {
-    if (s.length > n) {
-      return (s.substring(0, n) + '...');
+    if (s) {
+        if (s.length > n) {
+            return (s.substring(0, n) + '...');
+        } else {
+            return s;
+        }
     } else {
-      return s;
+        return s;
     }
-  } else {
-    return s;
-  }
 }
 
 export function isValidEmail(email) {
-  let emailRegex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-  let checkEmail = email.toLowerCase().match(emailRegex);
-  if (checkEmail === null) return false;
-  return true;
+    let emailRegex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    let checkEmail = email.toLowerCase().match(emailRegex);
+    if (checkEmail === null) return false;
+    return true;
 }
 
 export function isValidFlashAddress(value) {
-  try {
-    let address = Address.fromBase58Check(value);
-    if (address.version === NETWORK.pubKeyHash || address.version === NETWORK.scriptHash) {
-      return true;
-    } else {
-      return false;
+    try {
+        let address = Address.fromBase58Check(value);
+        if (address.version === NETWORK.pubKeyHash || address.version === NETWORK.scriptHash) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (e) {
+        return false;
     }
-  } catch (e) {
-    return false;
-  }
 }
 
 export function filterNumberEdit(event) {
-  let charCode = parseInt(event.charCode);
-  let keyCode = parseInt(event.keyCode);
-  let isValidAmountCharCode = ((charCode >= 48 && charCode <= 57) || keyCode == 8 || keyCode == 9 || keyCode == 127);
-  if (!isValidAmountCharCode) {
-    event.preventDefault ? event.preventDefault() : event.returnValue = false;
-  } else {
-    event.returnValue = true;
-  }
+    let charCode = parseInt(event.charCode);
+    let keyCode = parseInt(event.keyCode);
+    let isValidAmountCharCode = ((charCode >= 48 && charCode <= 57) || keyCode == 8 || keyCode == 9 || keyCode == 127);
+    if (!isValidAmountCharCode) {
+        event.preventDefault ? event.preventDefault() : event.returnValue = false;
+    } else {
+        event.returnValue = true;
+    }
 }
