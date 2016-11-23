bdc6cad6ab83ebab7cce53fd1ea43ffdff48ff32
Ba Ban <sandgrain@unseen.is>
2016-11-23 19:44:39 +0700

    Add server

---
+++ flashcoin.js
@@ -0,0 +1,9 @@
+// Flahcoin server
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/public'));
+var port = 8080;
+
+app.listen(port, function () {
+  console.log('Flashcoin listening on port!', port);
+});
