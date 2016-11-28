5d0f1a86e8c4ab9cd6a0fb28b5fc813807701d31
Tran Son <thaison@unseen.is>
2016-11-28 15:25:52 +0700

    using gzip

--- flashcoin.js
+++ flashcoin.js
@@ -1,6 +1,8 @@
 // Flahcoin server
 var express = require('express');
 var app = express();
+var compression = require('compression');
+app.use(compression());
 app.use(express.static(__dirname + '/public'));
 var port = 8080;
 
