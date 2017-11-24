82e2b91504faa162241c1215b76e72078cca713c
Maulik Vora <maulik_vr@yahoo.co.in>
2017-11-24 19:16:15 +0530

    wallet landing page changed to change the language instantly

---
+++ assets/lib/localize-index.js
@@ -0,0 +1,48 @@
+var textsIndex = null;
+
+function initIndexLocal(cb) {
+    var id_lang = localStorage.getItem('id_lang');
+    if(!id_lang) {
+      id_lang = 'en';
+    }
+    var lang_file = "assets/locale/" + id_lang +".json";
+	console.log(lang_file);
+    /*$.getJSON(lang_file, function(json) {
+        textsIndex = json;
+    });*/
+	
+	$.ajax({
+	  url: lang_file,
+	  dataType: 'json',
+	  async: false,
+	  data: {},
+	  success: function(json) {
+		textsIndex = json;
+	  }
+	});
+}
+
+function writeTextIndexLocale(key, params = {}, returnStr = false) {
+	
+	//setTimeout(function(){ 
+		if (!textsIndex[key]) {
+			if(!returnStr)
+				document.write(key);
+			else
+			return key;
+		}
+
+		var str = textsIndex[key];
+
+		for (let p in params) {
+			var sign = "%(" + p + ")s";
+			str = str.replace(sign, params[p]);
+		}
+
+		if(!returnStr)
+			document.write(str);
+		else
+			return str;
+	
+	//}, 2000);
+}
