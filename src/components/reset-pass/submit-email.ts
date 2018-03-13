c2b76ae54c143c9890952fc24dc8ebda88ebc4dc
Maulik Vora <maulik_vr@yahoo.co.in>
2018-03-13 12:34:08 +0530

    Google Recaptcha added for forgot password form

--- src/components/reset-pass/submit-email.ts
+++ src/components/reset-pass/submit-email.ts
@@ -12,6 +12,7 @@ import BaseElement from '../base-element';
 export default class SubmitEmail extends BaseElement {
   private isVerifyEmailSent = false;
   private static unsubscribe = null;
+  private captchaId = null;
 
   constructor() {
     super();
@@ -19,6 +20,17 @@ export default class SubmitEmail extends BaseElement {
     SubmitEmail.unsubscribe = store.subscribe(
       this.onApplicationStateChanged.bind(this)
     );
+  }
+
+  mounted() {
+    var self = this;
+    $.getScript('https://www.google.com/recaptcha/api.js?render=explicit', function () {
+      setTimeout(function() {
+        self.captchaId = grecaptcha.render('gcaptcha', {
+            'sitekey': '6LcMRCgTAAAAAAsGwyHN0EF4zp_vZzVJKMRS5I8C'
+        });
+      }, 500);
+    })
   }
 
   onApplicationStateChanged() {
@@ -44,6 +56,12 @@ export default class SubmitEmail extends BaseElement {
       return;
     }
 
+    let captchaResp = grecaptcha.getResponse(this.captchaId);
+    if (!captchaResp || captchaResp.length == 0) {
+        super.showError('',this.getText('signup_invalid_captcha'));
+        return;
+    }
+
     let clientHost = window.location.host;
 
     if (!clientHost || clientHost.length === 0) {
@@ -52,6 +70,7 @@ export default class SubmitEmail extends BaseElement {
 
     let params = {
       email: email,
+      g_recaptcha_response: captchaResp,
       callbackUrl: `http://${clientHost}/home.html#reset_password?token=`,
     };
     //riot.route("reset_password?token=");
