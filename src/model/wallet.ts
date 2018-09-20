e96e1b3c763ce0f9d6fa6cf93a40a832b032bde4
Ashvin <ashvinvanani@gmail.com>
2018-09-20 15:19:06 +0530

    fixes - REP coin removed

--- src/model/wallet.ts
+++ src/model/wallet.ts
@@ -250,9 +250,9 @@ export const NETWORKS = {
   ANT: {
     contract_address: '0x960b236A07cf122663c4303350609A66A7B288C0',
   },
-  REP: {
-    contract_address: '0x1985365e9f78359a9B6AD760e32412f4a445E862',
-  },
+  // REP: {
+  //   contract_address: '0x1985365e9f78359a9B6AD760e32412f4a445E862',
+  // },
   BNT: {
     contract_address: '0x1f573d6fb3f13d689ff844b4ce37794d79a7ff1c',
   },
